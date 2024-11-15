27a2a276ca606209f0069fa6ed04d3e4af9dc704
cleanerkkk <1963795880@qq.com>
2024-11-15 22:11:26 +0800

    ok

--- app/src/main/java/com/example/bottomnavigation1/model/GenerateRequest.kt
+++ app/src/main/java/com/example/bottomnavigation1/model/GenerateRequest.kt
@@ -7,7 +7,5 @@ data class GenerateRequest(
     val negativePrompt: String,
     val guidanceScale: Double = 7.5, //相关性
     val numInferenceSteps: Int = 50, //步数
-    val height: Int = 512,  // 默认 512x512 图像大小
-    val width: Int = 512,
     val seed: Int? = null   // 可选的种子，默认为 null
 )
