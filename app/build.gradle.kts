b50cfe8f790456246cbea409a7a4ea3e113dfe51
Alan10086 <x2584087792@163.com>
2024-11-15 12:28:21 +0800

    pic2pic update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
