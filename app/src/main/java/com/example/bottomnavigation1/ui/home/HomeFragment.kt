b2532ad8dd0c63ca285f6862849fc76cf9240785
Alan10086 <x2584087792@163.com>
2024-11-15 15:42:52 +0800

    pic2pic update

--- app/src/main/java/com/example/bottomnavigation1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/bottomnavigation1/ui/home/HomeFragment.kt
@@ -1,17 +1,37 @@
 package com.example.bottomnavigation1.ui.home
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.bottomnavigation1.databinding.FragmentHomeBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private var savedText: String? = null
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val imageUri: Uri? = data?.data
+            if (imageUri != null) {
+                loadImageFromUri(imageUri)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +42,10 @@ class HomeFragment : Fragment() {
 
         binding.toggleButton.setOnClickListener {
             showInputDialog()
+        }
+
+        binding.button.setOnClickListener {
+            openGallery()
         }
 
         return root
@@ -37,6 +61,25 @@ class HomeFragment : Fragment() {
 
     fun onInputDialogSave(inputText: String) {
         savedText = inputText
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        pickImage.launch(intent)
+    }
+
+    private fun loadImageFromUri(imageUri: Uri) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val inputStream = requireContext().contentResolver.openInputStream(imageUri)
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                withContext(Dispatchers.Main) {
+                    binding.imageView.setImageBitmap(bitmap)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
     override fun onDestroyView() {
