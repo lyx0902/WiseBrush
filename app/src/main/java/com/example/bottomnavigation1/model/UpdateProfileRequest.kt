56bafff87ce08eb1d35ec147c3d2144e91eb3e90
cleanerkkk <1963795880@qq.com>
2024-11-22 15:03:09 +0800

    ok

--- app/src/main/java/com/example/bottomnavigation1/model/UpdateProfileRequest.kt
+++ app/src/main/java/com/example/bottomnavigation1/model/UpdateProfileRequest.kt
@@ -2,7 +2,7 @@ package com.example.bottomnavigation1.model
 
 data class UpdateProfileRequest(
     val name: String,
-    val newName : String,
-    val newPassword :String,
-    val newEmail : String
+    val new_name : String,
+    val new_password :String,
+    val new_email : String
 )
